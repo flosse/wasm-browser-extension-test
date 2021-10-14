ea60b4615bb317663c095abf8132ccd26dfa682b
Markus Kohlhase <markus.kohlhase@slowtec.de>
2021-10-14 23:06:48 +0200

    Add log statements to content script

--- lib.js
+++ lib.js
@@ -1,10 +1,16 @@
+console.log("Run content script");
+
 const WASM_LIB_URL = chrome.runtime.getURL("lib.wasm");
+
+console.log("Load WASM module");
 
 fetch(WASM_LIB_URL)
   .then(response => response.arrayBuffer())
   .then(bytes => WebAssembly.instantiate(bytes))
   .then(results => {
+    console.log("Successfully instantiated WASM module");
     instance = results.instance;
+    console.log("Invoke 'add' function of WASM module");
     const result = instance.exports.add(41,1);
     console.log(result);
   })
